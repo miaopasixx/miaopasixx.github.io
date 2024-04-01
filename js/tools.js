582d2c4c0674150b08138daa204adac173d745c3
miaopasixx <a13026588708@163.com>
2024-04-01 22:21:13 +0800

    Site updated: 2024-04-01 22:21:12

--- js/tools.js
+++ js/tools.js
@@ -1,16 +1,16 @@
 // 在 tools.js 文件中添加以下代码：
 document.getElementById('category-select').addEventListener('change', function(e) {
-    const selectedCategory = e.target.value;
-    const items = document.querySelectorAll('.char .item');
-  
-    items.forEach(item => {
-      const category = item.dataset.category;
-  
-      // 根据所选类别显示或隐藏项目
-      if (selectedCategory === '' || selectedCategory === category) {
-        item.style.display = '';
-      } else {
-        item.style.display = 'none';
-      }
-    });
+  const selectedCategory = e.target.value;
+  const items = document.querySelectorAll('.char .item');
+
+  items.forEach(item => {
+    const category = item.dataset.category;
+
+    // 根据所选类别显示或隐藏项目
+    if (selectedCategory === '' || selectedCategory === category) {
+      item.style.display = '';
+    } else {
+      item.style.display = 'none';
+    }
   });
+});
